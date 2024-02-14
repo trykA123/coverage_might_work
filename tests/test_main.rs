dad8e5b5a57412aac5e791464445d634634f5f58
Claudiu Tancau <claudiu.tancau@elektrobit.com>
2024-02-14 09:24:55 +0200

    ads

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -11,12 +11,3 @@ fn test_add2() {
     assert_eq!(add(4, 3), 7);
 }
 
-#[test]
-fn test_subtract() {
-    assert_eq!(subtract(5, 3), 2);
-}
-
-#[test]
-fn test_multiply() {
-    assert_eq!(multiply(4, 5), 20);
-}
